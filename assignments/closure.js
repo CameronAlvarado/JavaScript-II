2e177785e17c2c1e6ec34c585debfa4994408e4c
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-07-23 19:31:41 -0700

    Completed all assignments for JS-II

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const closurePracticeAdd = (param1, param2) => {
+  return (param1 * param2);
+};
 
+let closurePracticeSub = (param1, param2) => {
+  return (param1 + param2);
+};
+
+let closurePractice = () => {
+    return closurePracticeAdd(2, 5) / closurePracticeSub(1, 1)
+};
+
+console.log(closurePractice());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
